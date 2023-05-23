b62e7b9c25ec692a94a9abbb9c04eb9cf574376b
drebob24 <abobbit@gmail.com>
2023-05-23 16:05:43 +0300

    Create function(s) to build a grid
    Create buildRow() and buildGrid() functions that are easily scalable
    to build any configuration of rows and columns.
    
    Next step is to scale the grid larger and add hover effects.

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const container = document.querySelector('.container');
 
-const grid = document.createElement('div');
-grid.classList.add('box');
-container.appendChild(grid);
+buildGrid();
 
+function buildGrid(){
+    for (let i = 0; i < 4; i++){
+        const grid = document.createElement('div');
+        grid.classList.add('row');
+        container.appendChild(grid);
+        buildRow(grid);
+    }
+}
+
+function buildRow(row){
+    for (let j = 0; j < 4; j++){
+        const box = document.createElement('div');
+        box.classList.add('box');
+        row.appendChild(box);
+    }
+}
