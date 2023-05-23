2a32f9b01631015889c1481b106bd572526c398a
drebob24 <abobbit@gmail.com>
2023-05-23 16:31:51 +0300

    Create colorBox function
    colorBox changes the color of the box when the mouse is hovering over
    it's div.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const container = document.querySelector('.container');
 buildGrid();
 
 function buildGrid(){
-    for (let i = 0; i < 4; i++){
+    for (let i = 0; i < 16; i++){
         const grid = document.createElement('div');
         grid.classList.add('row');
         container.appendChild(grid);
@@ -12,9 +12,20 @@ function buildGrid(){
 }
 
 function buildRow(row){
-    for (let j = 0; j < 4; j++){
+    for (let j = 0; j < 16; j++){
         const box = document.createElement('div');
         box.classList.add('box');
         row.appendChild(box);
     }
+}
+
+colorBox();
+
+function colorBox(){
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.addEventListener('mouseover', () => {
+            box.style.backgroundColor = 'black';
+        });   
+    });
 }
